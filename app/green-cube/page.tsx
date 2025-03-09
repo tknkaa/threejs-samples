9f6f92d88280e93b51606c0a5bd6e9976263eb3c
tknkaa <tanaka-ryohei@g.ecc.u-tokyo.ac.jp>
2025-03-09 17:00:20 +0900

    feat: separate art works in different pages

---
+++ app/green-cube/page.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import Link from "next/link";
+import { Suspense, useEffect, useRef } from "react";
+import * as THREE from "three";
+
+export default function Home() {
+	const mountRef = useRef<HTMLDivElement | null>(null);
+
+	useEffect(() => {
+		if (!mountRef.current) return;
+		const scene = new THREE.Scene();
+		const camera = new THREE.PerspectiveCamera(
+			75,
+			window.innerWidth / window.innerHeight,
+			0.1,
+			1000,
+		);
+		const renderer = new THREE.WebGLRenderer();
+
+		renderer.setSize(window.innerWidth, window.innerHeight);
+		mountRef.current.appendChild(renderer.domElement);
+
+		const geometry = new THREE.BoxGeometry(1, 1, 1);
+		const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+		const cube = new THREE.Mesh(geometry, material);
+		scene.add(cube);
+
+		camera.position.z = 5;
+
+		const animate = () => {
+			requestAnimationFrame(animate);
+			cube.rotation.x += 0.01;
+			cube.rotation.y += 0.01;
+			renderer.render(scene, camera);
+		};
+		animate();
+
+		return () => {
+			if (mountRef.current) {
+				mountRef.current.removeChild(renderer.domElement);
+			}
+			renderer.dispose();
+		};
+	}, []);
+
+	return (
+		<>
+			<div className="text-white bg-black ">
+				<Link href={"/"}>Back to Home</Link>
+				<div ref={mountRef}></div>
+			</div>
+		</>
+	);
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,47 +1,16 @@
-"use client";
-
-import { useEffect, useRef } from "react";
-import * as THREE from "three";
+import Link from "next/link";
 
 export default function Home() {
-	const mountRef = useRef<HTMLDivElement | null>(null);
-
-	useEffect(() => {
-		if (!mountRef.current) return;
-		const scene = new THREE.Scene();
-		const camera = new THREE.PerspectiveCamera(
-			75,
-			window.innerWidth / window.innerHeight,
-			0.1,
-			1000,
-		);
-		const renderer = new THREE.WebGLRenderer();
-
-		renderer.setSize(window.innerWidth, window.innerHeight);
-		mountRef.current.appendChild(renderer.domElement);
-
-		const geometry = new THREE.BoxGeometry(1, 1, 1);
-		const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-		const cube = new THREE.Mesh(geometry, material);
-		scene.add(cube);
-
-		camera.position.z = 5;
-
-		const animate = () => {
-			requestAnimationFrame(animate);
-			cube.rotation.x += 0.01;
-			cube.rotation.y += 0.01;
-			renderer.render(scene, camera);
-		};
-		animate();
-
-		return () => {
-			if (mountRef.current) {
-				mountRef.current.removeChild(renderer.domElement);
-			}
-			renderer.dispose();
-		};
-	}, []);
-
-	return <div ref={mountRef} />;
+	return (
+		<>
+			<div className="mb-2 text-lg font-semibold text-gray-900 dark:text-white">
+				Three.js samples
+			</div>
+			<ul>
+				<li className="max-w-md space-y-1 text-gray-500 list-disc list-inside dark:text-gray-400">
+					<Link href={"/green-cube"}>green cube</Link>
+				</li>
+			</ul>
+		</>
+	);
 }
